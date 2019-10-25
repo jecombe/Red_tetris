9fdd17c15a3e48b3e0c0966ed5a6cb033be78a87
Jeremy Combe <jecombe@z3r3p2.le-101.fr>
2019-10-25 20:39:41 +0200

    check full line ok

--- src/client/hooks/useStage.js
+++ src/client/hooks/useStage.js
@@ -2,17 +2,49 @@
 import { useState, useEffect } from 'react';
 import { createStage } from '../helpers/gameHelpers';
 
+
+function is_full(currentValue)
+{
+  return (currentValue[1] === 'merged');
+
+}
 export const useStage = (player, resetPlayer) => {
+
   const [stage, setStage] = useState(createStage());
-  console.log('useStage');
+
+  const [clear, setClearRows] = useState(0);
 
   useEffect(() => {
+
+    setClearRows(0);
+
+   const updateRows = (newStage) => {
+
+    //Pour la hauteur verifie si une ligne est pleine
+      newStage.forEach((row) => {
+
+        let full_line = row.every(is_full)
+        if (full_line === true)
+        {
+          //Check l'index de la ligne pleine;
+          let index = newStage.indexOf(row);
+          //Met la ligne a 0
+          row.fill([0, 'clear']);
+          //Supprime la ligne avec l'index et decalle e tableau, il restera non pas 20 de hauteur mais 19
+          newStage.splice(index, 1);
+          //Ajoute au debut du tableau un nouveau tableau de 10 a 0
+          newStage.unshift(new Array(10).fill([0, 'clear']));
+        }
+
+      });
+
+      return (newStage)
+    }
+
     const updateStage = (prevStage) => {
-      console.log('updateStage', prevStage, '================+> ', player);
       // First flush the stage
       const newStage = prevStage.map((row) => row.map((cell) => (cell[1] === 'clear' ? [0, 'clear'] : cell)));
 
-      console.log('-----------------------+> ', player);
       player.tetromino.forEach((row, y) => {
         row.forEach((value, x) => {
           if (value !== 0) {
@@ -23,10 +55,9 @@ export const useStage = (player, resetPlayer) => {
           }
         });
       });
-      // Then check if we collided
       if (player.collided) {
-        console.log('=======> AHHDHDHDHDHDHD');
         resetPlayer();
+        return updateRows(newStage);
       }
 
       return newStage;
@@ -35,5 +66,5 @@ export const useStage = (player, resetPlayer) => {
     setStage((prev) => updateStage(prev));
   }, [player, resetPlayer]);
 
-  return [stage, setStage];
+  return [stage, setStage, setClearRows];
 };
--- src/client/hooks/useStageSmall.js
+++ src/client/hooks/useStageSmall.js
@@ -4,15 +4,12 @@ import { createStageSmall } from '../helpers/gameHelpers';
 
 export const useStageSmall = (player, resetPlayerSmall) => {
   const [stage_small, setStageSmall] = useState(createStageSmall());
-  console.log('useStage');
 
   useEffect(() => {
     const updateStage = (prevStage) => {
-      console.log('updateStage', prevStage, '================+> ', player);
       // First flush the stage
       const newStage = prevStage.map((row) => row.map((cell) => (cell[1] === 'clear' ? [0, 'clear'] : cell)));
 
-      console.log('-----------------------+> ', player);
       player.tetromino.forEach((row, y) => {
         row.forEach((value, x) => {
           if (value !== 0) {

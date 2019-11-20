99e35cf58fef3e31ce2232416d58bd9acf0a3b0c
dz0nda <dzonda@student.42.fr>
2019-11-20 19:57:52 +0100

    - refactor: header

---
+++ src/client/components/headerBar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import logo from '../img/header3.png';
+
+const HeaderBar = props => {
+    let { playerName, playerRoom } = props;
+    
+    if (!playerName) playerName = "Anonymous"
+
+    if (!playerRoom) playerRoom = "You are not in a room !"
+    else playerRoom = `You are in ${playerRoom} room !`
+
+    return (
+        <div>
+            <div style={style.headerInfoUserStyle}>
+                Welcome, {playerName}!
+                <br />
+                {playerRoom}
+            </div>
+            <div style={style.headerLogoStyle}>
+                <img style={headerLogoImgStyle} src={logo} alt="Logo" />
+            </div>
+            <div style={style.headerInfoSocketStyle}>
+                Socket connection
+            </div>
+        </div>
+    );
+};
+
+const style = {
+    headerLogoStyle: {
+        // display: "flex",
+        // justifyContent: "center",
+        // padding: '0.5%',
+        // width: '100%',
+        border: '1px solid black'
+    },
+    headerLogoImgStyle: {
+        width: '20%'
+    },
+    headerInfoSocketStyle: {
+        //   order: '0',
+        //   flex: '0 1 auto',
+        //   alignSelf: 'auto',
+        border: '1px solid black'
+    }
+}
+
+export default HeaderBar;
--- src/client/containers/header.js
+++ src/client/containers/header.js
@@ -1,49 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import HeaderLogo from '../components/headerLogo';
-import HeaderInfoUser from '../components/headerInfoUser';
-import HeaderInfoSocket from '../components/headerInfoSocket';
+import HeaderBar from '../components/headerBar';
 
 const Header = props => {
   return (
-    <div style={headerStyle}>
-        <HeaderInfoUser
-          playerName={props.playerName} 
-          playerRoom={props.playerRoom}
-        />
-        <HeaderLogo />
-        <HeaderInfoSocket />
-    </div>
+    <HeaderBar 
+      playerName={props.playerName} 
+      playerRoom={props.playerRoom} 
+    />
   );
-}
-
-const headerStyle = {
-	display: 'flex',
-	flexDirection: 'row',
-	flexWrap: 'nowrap',
-	justifyContent: 'space-between',
-	alignItems: 'baseline',
-	alignContent: 'stretch'
-}
-
-// const headerInfoStyle = {
-//   order: '0',
-//   flex: '0 1 auto',
-//   alignSelf: 'auto',
-//   border: '1px solid black'
-// }
-
-// const headerLogoStyle = {
-//   order: '0',
-//   flex: '1 0 auto',
-//   alignSelf: 'auto',
-//   border: '1px solid black'
-// }
+};
 
 const mapStateToProps = state => ({
 	playerName: state.player.playerName,
 	playerRoom: state.player.playerRoom
 });
 
-export default connect(mapStateToProps, null)(Header);
+export default connect(mapStateToProps)(Header);

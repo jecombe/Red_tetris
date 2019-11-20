8c823e953c4d1fc1e797e8642f34046c8bd577c3
Jeremy Combe <jeremcombe@hotmail.fr>
2019-11-20 22:05:27 +0100

    param socket.io -> fix random disconnect

--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import http from 'http';
-import socketIO from 'socket.io';
+//import socketIO from 'socket.io';
 
 import appRoutes from './routes/app';
 import socketHandler from './socket';
 
+
 const port = 8000;
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+//const io = socketIO(server);
+const io = require('socket.io')(server, {
+    pingInterval: 5000,
+    pingTimeout: 15000,
 
+});
 var userlist = [];
 let rooms = [];
 

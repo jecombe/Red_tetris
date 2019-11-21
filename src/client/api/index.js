8c374c874b96a4824607c7f9fa56efd4de0886bf
dz0nda <dzonda@student.42.fr>
2019-11-21 10:49:17 +0100

    - update: socket disconnect ES6 for server && setup in client

--- src/client/api/index.js
+++ src/client/api/index.js
@@ -1,6 +1,9 @@
 import socketIOClient from "socket.io-client";
 
 const PORT = 8000;
-const socket = socketIOClient(`http://localhost:${PORT}`);
+const socket = socketIOClient(`http://localhost:${PORT}`, {
+    pingInterval: 5000,
+    pingTimeout: 15000
+});
 
 export default socket;
--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import http from 'http';
-//import socketIO from 'socket.io';
+import socketIO from 'socket.io';
 
 import appRoutes from './routes/app';
 import socketHandler from './socket';
@@ -9,12 +9,11 @@ import socketHandler from './socket';
 const port = 8000;
 const app = express();
 const server = http.createServer(app);
-//const io = socketIO(server);
-const io = require('socket.io')(server, {
+const io = socketIO(server, {
     pingInterval: 5000,
     pingTimeout: 15000,
-
 });
+
 var userlist = [];
 let rooms = [];
 

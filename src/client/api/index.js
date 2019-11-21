9886023a8056708a60c438c3276436a2b33f80bb
Jeremy Combe <jecombe@z1r9p1.le-101.fr>
2019-11-21 13:45:17 +0100

    delete option client useless

--- src/client/api/index.js
+++ src/client/api/index.js
@@ -1,9 +1,5 @@
 import socketIOClient from "socket.io-client";
 
 const PORT = 8000;
-const socket = socketIOClient(`http://localhost:${PORT}`, {
-    pingInterval: 5000,
-    pingTimeout: 15000
-});
-
+const socket = socketIOClient(`http://localhost:${PORT}`)
 export default socket;

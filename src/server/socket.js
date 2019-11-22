af09cbcacdd81ee9aa04c3afae18e565dc7aea11
Jeremy Combe <jecombe@z3r5p3.le-101.fr>
2019-11-22 16:59:44 +0100

    fix join and emit room in socket.js

--- src/server/socket.js
+++ src/server/socket.js
@@ -1,6 +1,6 @@
 import { loginUser, playerLogin } from './handlers/PlayerHandler';
 import { roomJoin } from './handlers/RoomHandler';
-import { createGame, freeUserInGame, startGame , searchUserInList, searchRoomInUser} from './handlers/GameHandler';
+import { createGame, freeUserInGame, startGame, searchUserInList, searchRoomInUser } from './handlers/GameHandler';
 
 const socketHandler = (io, userlist, rooms) => {
 
@@ -22,11 +22,11 @@ const socketHandler = (io, userlist, rooms) => {
             console.log('ONE: created user before game start ', objPlayerBeforeGame)
             /*Create or join game if existe*/
             const [objGame, objPlayerAfterGame] = createGame(rooms, userlist, game.username, game.roomActual)
-            console.log('- TWO: create game and add info in object Player') 
+            console.log('- TWO: create game and add info in object Player')
             console.log('- ObjectGame -> ', objGame)
-            console.log('- Object PLayer after join or create game -> ' ,objPlayerAfterGame)
+            console.log('- Object PLayer after join or create game -> ', objPlayerAfterGame)
             /*Join room*/
-            socket.join(game.gameName)
+            socket.join(game.roomActual)
 
             /*A definir*/
             io.sockets.emit('joined', {
@@ -34,26 +34,29 @@ const socketHandler = (io, userlist, rooms) => {
                 'rooms': rooms
             });
         })
-        
+
         socket.on('startGame', game => {
-            console.log('RESPONSE: ', game)
+            console.log('RESPONSE: ', game.room)
 
             let piece = startGame(game, rooms)
             io.sockets.in(game.room).emit('pieceStart', piece);
 
+
+
+
         })
 
-        socket.on('disconnect', () => {
-            console.log('LIST ROOM BEFORE ', rooms, 'LIST USER BEFORE ', userlist)
-            /*Search user login in userList*/
-            let login = searchUserInList(socket.id, userlist)
-            /*Search room name of player*/
-            let roomActual = searchRoomInUser(userlist, login)
-            socket.leave(roomActual)
-            freeUserInGame(login, roomActual, rooms, userlist)
-            console.log('LIST ROOM AFTER ', rooms, 'LIST USER AFTER ', userlist)
-        });
+    socket.on('disconnect', () => {
+        console.log('LIST ROOM BEFORE ', rooms, 'LIST USER BEFORE ', userlist)
+        /*Search user login in userList*/
+        let login = searchUserInList(socket.id, userlist)
+        /*Search room name of player*/
+        let roomActual = searchRoomInUser(userlist, login)
+        socket.leave(roomActual)
+        freeUserInGame(login, roomActual, rooms, userlist)
+        console.log('LIST ROOM AFTER ', rooms, 'LIST USER AFTER ', userlist)
     });
+});
 }
 
 module.exports = socketHandler;

d5aac84ba2858f98dc6e7e8cba46a6fbc058b07b
Jeremy Combe <jecombe@z3r5p3.le-101.fr>
2019-11-22 13:15:38 +0100

    receipt piece from server in client

--- src/server/stage.js
+++ src/server/stage.js
@@ -2,8 +2,6 @@
 export const STAGE_WIDTH = 10;
 export const STAGE_HEIGHT = 20;
 
-export const STAGE_WIDTH_SMALL = 8;
-export const STAGE_HEIGHT_SMALL = 8;
 
 export const createStage = () =>
   Array.from(Array(STAGE_HEIGHT), () =>

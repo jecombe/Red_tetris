25813e8c05f5ecb6fb3ee9e88f0524dd7a22d9e9
dz0nda <dzonda@student.42.fr>
2019-12-06 13:38:58 +0100

    - add: logger

--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,22 @@
 import express from 'express';
 import http from 'http';
 import socketIO from 'socket.io';
+import morgan from 'morgan';
+import logger from './helpers/logger';
 
 import appRoutes from './routes/app';
 import socketHandler from './socket';
+import params from '../../params';
 
+const { host, port, url } = params.server;
 
-const port = 8000;
-const app = express();
+const app = express( );
 const server = http.createServer(app);
 const io = socketIO(server, {
   pingInterval: 5000,
   pingTimeout: 15000,
 });
+
 
 const connections = [];
 
@@ -20,13 +24,14 @@ const userlist = [];
 const rooms = [];
 
 app.use(appRoutes);
+app.use(morgan('combined'));
 
 io.use((socket, next) => {
   connections.push(socket.id);
-  console.log(connections);
+  logger.info(connections);
   return next();
 });
 
 socketHandler(io, userlist, rooms);
 
-server.listen(port, () => console.log(`Running on localhost:${port}`));
+server.listen({port, host}, () => logger.info(`Running on ${url}`));

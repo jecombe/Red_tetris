48aa94d25de6dd2562785c7ea2e41f9a3ef3508a
dz0nda <dzonda@student.42.fr>
2019-12-08 17:38:06 +0100

    - minor: fix-lint

--- src/client/components/Footer/FooterLayout.js
+++ src/client/components/Footer/FooterLayout.js
@@ -9,5 +9,4 @@ const FooterLayout = () => (
   </Grid>
 );
 
-
 export default FooterLayout;
--- src/client/components/Login/LoginLayout.js
+++ src/client/components/Login/LoginLayout.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
+import { makeStyles } from '@material-ui/core/styles';
 
 import LoginForm from './LoginForm';
 import LoginRooms from './LoginRooms';

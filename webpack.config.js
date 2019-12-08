aac188f6dadca82603f8a6b10df296e9231f2b37
dz0nda <dzonda@student.42.fr>
2019-12-08 17:36:24 +0100

    - fix: lint

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,58 @@
-const path = require("path");
+const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: path.join(__dirname, "src", "client", "index.js"),
-    module: {
-        rules: [
+  entry: path.join(__dirname, 'src', 'client', 'index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
           {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader"
-            }
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
           },
-          {
-            test: /\.css$/,
-            exclude: /node_modules/,
-            use: [
-              'style-loader',
-              {
-                loader: 'css-loader',
-                options: {
-                  modules: true,
-                },
-              },
-            ],
-          },
-          {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [
-              'file-loader',
-            ],
-          },
-          {
-            test: /\.(woff|woff2|eot|ttf|otf)$/,
-            use: [
-              'file-loader',
-            ],
-          },
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-
-    output: {
-        path: path.join(__dirname, "public"),
-        filename: "bundle.js",
-        publicPath: "/"
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
     ],
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        hot: true,
-        historyApiFallback: true
-    }
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    hot: true,
+    historyApiFallback: true,
+  },
 };
-  

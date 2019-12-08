26196707dc6a191fc733f61207454eaf3180d846
dz0nda <dzonda@student.42.fr>
2019-12-08 17:27:50 +0100

    - add: Footer material-ui component

---
+++ src/client/components/Footer/FooterLayout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+
+import FooterAuthors from './FooterAuthors';
+
+const FooterLayout = () => (
+  <Grid container justify="center" alignItems="center">
+    <FooterAuthors />
+  </Grid>
+);
+
+
+export default FooterLayout;

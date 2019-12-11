fd34e3bbdd23f535a4cad77303a2ce3e8c49306e
dz0nda <dzonda@student.42.fr>
2019-12-11 13:48:33 +0100

    - [SETUP-TEST] add: events

--- src/server/utils/events.js
+++ src/server/utils/events.js
@@ -1,11 +1,18 @@
 module.exports = {
   CONNECT: 'connect',
-
   DISCONNECT: 'disconnect',
+  ERROR: 'error',
+
+  LOGIN: 'login',
+  START_GAME: 'startGame',
+  POSITION_TETRO: 'positionTetro',
 
   com_ECHO: '#echo',
   res_ECHO: '@echo',
 
   com_BELLO: '#bello',
   res_BELLO: '@bello',
+
+  com_ROOMS: '#rooms',
+  res_ROOMS: '@rooms',
 };

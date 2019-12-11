3278b008e2eb952ef54b9feb5ab0b2f579563068
Jeremy Combe <jecombe@z3r5p3.le-101.fr>
2019-12-11 17:52:02 +0100

    resize stage

--- src/client/components/Game/StyledOtherStage.js
+++ src/client/components/Game/StyledOtherStage.js
@@ -6,13 +6,13 @@ const StyledOtherStage = styled.div`
 display: grid;
 grid-template-rows: repeat(
   ${(props) => props.height},
-  calc(20vw / ${(props) => props.width})
+  calc(12vw / ${(props) => props.width})
 );
 grid-template-columns: repeat(${(props) => props.width}, 1fr);
 grid-gap: 1px;
 border: 4px solid red;
 width: 100%;
-max-width: 25vw;
+max-width: 12vw;
 background: #111;
 `;
 

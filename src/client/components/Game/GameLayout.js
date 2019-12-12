ad2f9e8947a2aa95a6109f28c06a2ec6de0ca127
dz0nda <dzonda@student.42.fr>
2019-12-12 01:39:08 +0100

    - [FRONT] update: refactor game component

--- src/client/components/Game/GameLayout.js
+++ src/client/components/Game/GameLayout.js
@@ -1,38 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Stage from './Stage';
-import StageTetro from './StageTetro';
 import GameStatus from './gameStatus';
 
-const GameLayout = (props) => {
-  const {
-    move,
-    playerStage,
-    playerNextPiece,
-    handleSubmitStatus
-  } = props;
-
-  return (
-    <Card style={style.gameLayout} onKeyDown={(e) => move(e)}>
-      {playerStage && playerStage.length &&
-        <Stage tabIndex="0" stage={playerStage} /> }
-      {playerNextPiece && playerNextPiece.length &&
-        <StageTetro stage={playerNextPiece} /> }
-      <GameStatus handleSubmit={handleSubmitStatus} />
-    </Card>
-  );
-};
-
-const style = {
+const useStyles = makeStyles((theme) => ({
   gameLayout: {
     display: 'flex',
     flexGrow: '1',
     justifyContent: 'space-around',
     alignItems: 'center',
     width: '100vw',
+    margin: theme.spacing(1),
+    padding: theme.spacing(1),
   },
+}));
+
+const GameLayout = (props) => {
+  const {
+    move,
+    playerStage,
+    playerNextPiece,
+    handleSubmitStatus,
+  } = props;
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.gameLayout} onKeyDown={(e) => move(e)}>
+      {playerStage && playerStage.length
+        && <Stage tabIndex="0" stage={playerStage} /> }
+      {playerNextPiece && playerNextPiece.length
+        && <Stage stage={playerNextPiece} /> }
+      <GameStatus handleSubmit={handleSubmitStatus} />
+    </Card>
+  );
 };
 
 GameLayout.propTypes = {
--- src/client/components/Game/Stage.js
+++ src/client/components/Game/Stage.js
@@ -1,14 +1,40 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import PropTypes from 'prop-types';
-import Cell from './Cellule';
-import StyledStage from './StyledStage';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Stage = ({ stage }) => (
-  <StyledStage width={stage[0].length} height={stage.length}>
-    {stage.map((row) => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
-  </StyledStage>
-);
+import Cell from './Cell';
+
+const useStyles = makeStyles(() => ({
+  stage: (props) => ({
+    display: 'grid',
+    gridTemplateRows: `repeat(
+      ${props.height},
+      calc(20vw / ${props.width}))`,
+    gridTemplateColumns: `repeat(${props.width}, 1fr)`,
+    gridGap: '1px',
+    border: '4px solid red',
+    width: '100%',
+    maxWidth: '25vw',
+    background: '#111',
+  }),
+}));
+
+const Stage = (props) => {
+  const { stage } = props;
+  const style = {
+    width: stage[0].length,
+    height: stage.length,
+  };
+  const classes = useStyles(style);
+
+  return (
+    <Box className={classes.stage}>
+      {stage.map((row) => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
+    </Box>
+  );
+};
 
 Stage.propTypes = {
   stage: PropTypes.arrayOf(PropTypes.string).isRequired,

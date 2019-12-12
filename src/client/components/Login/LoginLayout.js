c4d63e40031a0af82da3b2ffe9f7e9b01ffa4af9
dz0nda <dzonda@student.42.fr>
2019-12-12 01:38:33 +0100

    - [FRONT] minor: lint

--- src/client/components/Login/LoginLayout.js
+++ src/client/components/Login/LoginLayout.js
@@ -16,7 +16,6 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const LoginLayout = (props) => {
-  const classes = useStyles();
   const {
     handleSubmit,
     handlePlayerName,
@@ -26,6 +25,7 @@ const LoginLayout = (props) => {
     rooms,
     handleRoomSubmit,
   } = props;
+  const classes = useStyles();
 
   return (
     <Grid container direction="row" justify="center" alignItems="center">

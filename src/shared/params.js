ec1dde92afe359035f8260eba3786824eb28a59c
jecombe <jeremcombe@hotmail.fr>
2020-03-11 19:43:55 +0100

    change server ip

--- src/shared/params.js
+++ src/shared/params.js
@@ -3,7 +3,7 @@ module.exports = {
     id: 'CLIENT',
   },
   server: {
-    host: '10.3.7.4',
+    host: '0.0.0.0',
     port: 3000,
     get url() { return `http://${this.host}:${this.port}`; },
   },

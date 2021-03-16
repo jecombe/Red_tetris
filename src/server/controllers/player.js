5ac2245e456a2c04f15ce695eaa85e45b2484e53
dz0nda <dzonda@student.42.fr>
2021-03-16 13:23:53 +0100

    refactor: player controller

--- src/server/controllers/player.js
+++ src/server/controllers/player.js
@@ -7,66 +7,6 @@ import { calcScore, calcLevel, keys } from '../helpers/gameHelper';
 
 const gameAllowedKeys = [keys.KDOWN, keys.KLEFT, keys.KRIGHT, keys.KUP, keys.KSPACE];
 
-const resCollided = (req, res) => {
-  const { room, name, keyCode } = req.data;
-
-  try {
-    const Game = RedTetris.getGame(room);
-    if (!Game) throw new Error('Game not exists');
-
-    const Player = RedTetris.getGame(room).getPlayer(req.socket.id);
-    if (!Player) throw new Error('Player not exists');
-
-    Game.updateCollision(req.socket.id);
-
-    emitToRoom(res.io, room, ev.res_UPDATE_GAME_PLAYERS, {
-      status: 200,
-      payload: { id: req.socket.id, player: Player },
-    });
-
-    Player.setFlushUpdate();
-
-    logger.info('[resCollided]', 'success');
-  } catch (err) {
-    logger.error('[resCollided] ', err);
-  }
-};
-
-const resFinish = (req, res) => {
-  const { room, name, keyCode } = req.data;
-
-  try {
-    const Game = RedTetris.getGame(room);
-    if (!Game) throw new Error('Game not exists');
-
-    const Player = RedTetris.getGame(room).getPlayer(req.socket.id);
-    if (!Player) throw new Error('Player not exists');
-
-    Game.updateLoose(req.socket.id);
-
-    emitToSocket(req.socket, ev.res_START_GAME, {
-      status: 100,
-      payload: { message: Player.win === true ? 'You win!' : 'You loose!' },
-    });
-
-    if (Game.getStarted() === false) {
-      emitToRoom(res.io, room, ev.res_START_GAME, {
-        status: 100,
-        payload: { message: 'The game is finished' },
-      });
-
-      emitToRoom(res.io, room, ev.res_UPDATE_GAME_SETTINGS, {
-        status: 200,
-        payload: { settings: Game.settings },
-      });
-    }
-
-    logger.info('[resLoose]', 'success');
-  } catch (err) {
-    logger.error('[resLoose] ', err);
-  }
-};
-
 const resMove = (req, res) => {
   const { room, name, keyCode } = req.data;
 
@@ -81,8 +21,38 @@ const resMove = (req, res) => {
 
     Player.setMove(keyCode);
 
-    if (Player.getFinish() === true) resFinish(req, res);
-    else if (Player.getCollided() === true) resCollided(req, res);
+    /* Player finished */
+    if (Player.getFinish() === true) {
+      Game.updateLoose(req.socket.id);
+
+      emitToSocket(req.socket, ev.res_START_GAME, {
+        status: 100,
+        payload: { message: Player.win === true ? 'You win!' : 'You loose!' },
+      });
+
+      /* Game finished */
+      if (Game.getStarted() === false) {
+        emitToRoom(res.io, room, ev.res_START_GAME, {
+          status: 100,
+          payload: { message: 'The game is finished' },
+        });
+
+        emitToRoom(res.io, room, ev.res_UPDATE_GAME_SETTINGS, {
+          status: 200,
+          payload: { settings: Game.settings },
+        });
+      }
+      /* Player Collided */
+    } else if (Player.getCollided() === true) {
+      Game.updateCollision(req.socket.id);
+
+      emitToRoom(res.io, room, ev.res_UPDATE_GAME_PLAYERS, {
+        status: 200,
+        payload: { id: req.socket.id, player: Player },
+      });
+
+      Player.setFlushUpdate();
+    }
 
     emitToSocket(req.socket, ev.res_UPDATE_PLAYER, {
       status: 200,
@@ -92,7 +62,7 @@ const resMove = (req, res) => {
       },
     });
 
-    logger.info('[resMove]', 'success');
+    // logger.info('[resMove]', 'success');
   } catch (err) {
     logger.error('[resMove] ', err);
 
